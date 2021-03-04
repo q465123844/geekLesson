cb56b5bd0b4782381fe73e9e17c7c3cbd2f224b3
liufeng <liufeng@szmsd.com>
2021-03-04 17:51:11 +0800

    修改整体结构及web.xml文件

---
+++ project/user-platform/user-web/src/main/java/com/leo/geektimes/projects/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.leo.geektimes.projects.user.service.impl;
+
+import com.leo.geektimes.projects.user.domain.User;
+import com.leo.geektimes.projects.user.repository.InMemoryUserRepository;
+import com.leo.geektimes.projects.user.repository.UserRepository;
+import com.leo.geektimes.projects.user.service.UserService;
+
+/**
+ * @author leo
+ * @date 2021-03-03 22:49
+ */
+public class UserServiceImpl implements UserService {
+
+    private UserRepository repository = new InMemoryUserRepository();
+    @Override
+    public boolean register(User user) {
+        return repository.save(user);
+    }
+
+    @Override
+    public boolean deregister(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean update(User user) {
+        return false;
+    }
+
+    @Override
+    public User queryUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User queryUserByNameAndPassword(String name, String password) {
+        return null;
+    }
+}
